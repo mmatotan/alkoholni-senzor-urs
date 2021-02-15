365b7ad828617f04a0cf2ae2c43e8320e1e56595
Marko Matotan <marko.matotan4@gmail.com>
2021-02-15 11:22:46 +0100

    CSV support

--- projekt.py
+++ projekt.py
@@ -1,17 +1,34 @@
 import serial
-import io
-import time
+import datetime
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import dateutil
 
+# Specifikacija USARTa
 ser = serial.Serial('/dev/ttyUSB0')
 ser.baudrate = 9600
 ser.parity = serial.PARITY_EVEN
 ser.stopbits = serial.STOPBITS_TWO
 ser.bytesize = serial.EIGHTBITS
-print(ser.name)
+
+date = datetime.date.today()
+filename = f'{date.strftime("%d-%m-%Y")}.csv'
+csvFile = open(filename, "a+")
+fileWriter = csv.writer(csvFile)
+
+# Za smanjenje nepotrebnih podataka
+last_measure = -1
 
 while 1:
-    s = ser.readline()
-    print(s.decode("ascii"))
-    time.sleep(0.5)
+    rec = ser.readline()
+    rec = int(rec.decode("ascii"))
+    rec = rec
+    if(rec == last_measure):
+        continue
+    output_time = f'{datetime.datetime.now().strftime("%H:%M:%S:%f")[:-4]}'
+    print(f'{output_time}\t{rec}')
+    fileWriter.writerow([output_time] + [rec])
+    last_measure = rec
 
 ser.close()
